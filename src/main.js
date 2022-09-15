b2d69c744c0ab3501e78c7c31d8e701c8c44dcc7
Angel Velasquez <pcisavel@upc.edu.pe>
2022-09-15 15:34:57 -0500

    ui: Added PrimeVue UI Library Configuration and Component dependencies.

--- src/main.js
+++ src/main.js
@@ -1,14 +1,59 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import {createApp} from 'vue'
+import {createPinia} from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
 import './assets/main.css'
 
-const app = createApp(App)
+// Add PrimeVue
+import PrimeVue from "primevue/config";
 
-app.use(createPinia())
-app.use(router)
+// Add Theme
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
+import 'primevue/resources/themes/md-light-indigo/theme.css';
 
-app.mount('#app')
+// Add PrimeFlex
+import 'primeflex/primeflex.css';
+
+// Add Toast Service
+import ToastService from "primevue/toastservice";
+
+// Add UI Components
+import DataTable from "primevue/datatable";
+import Column from "primevue/column";
+import Toolbar from "primevue/toolbar";
+import InputText from "primevue/inputtext";
+import Textarea from "primevue/textarea";
+import Button from "primevue/button";
+import Row from "primevue/row";
+import Sidebar from "primevue/sidebar";
+import Menu from "primevue/menu";
+import Dialog from "primevue/dialog";
+import Toast from "primevue/toast";
+import Dropdown from "primevue/dropdown";
+import Tag from "primevue/tag";
+import Card from "primevue/card";
+
+
+createApp(App)
+    .use(createPinia())
+    .use(router)
+    .use(PrimeVue, { ripple: true })
+    .use(ToastService)
+    .component('pv-data-table', DataTable)
+    .component('pv-column', Column)
+    .component('pv-toolbar', Toolbar)
+    .component('pv-input-text', InputText)
+    .component('pv-text-area', Textarea)
+    .component('pv-button', Button)
+    .component('pv-row', Row)
+    .component('pv-sidebar', Sidebar)
+    .component('pv-menu', Menu)
+    .component('pv-dialog', Dialog)
+    .component('pv-toast', Toast)
+    .component('pv-dropdown', Dropdown)
+    .component('pv-tag', Tag)
+    .component('pv-card', Card)
+    .mount('#app')
