567ed4efb2ccfe544c797f286223e51048c54029
Angel Velasquez <pcisavel@upc.edu.pe>
2022-09-21 12:23:18 -0500

    chore: Optimized components loading and fixed components names.

---
+++ src/public/pages/about.component.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>This is an about page</h1>
+    <pv-button @click="navigateToHome">Home</pv-button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {}
+  },
+  methods: {
+    navigateToHome() {
+      this.$router.push('/home');
+    }
+  }
+}
+
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,29 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeComponent from '../public/pages/home.component.vue'
 
+// Lazy loading Components
+const TutorialsComponent = () => import('../learning/pages/tutorial-list.component.vue');
+const AboutComponent = () => import('../public/pages/about.component.vue');
+
+// Application Routes
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/home',
       name: 'home',
-      component: HomeView
+      alias: '/',
+      component: HomeComponent
     },
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: AboutComponent
     },
     {
       path: '/tutorials',
       name: 'tutorials',
-      component: () => import('../learning/pages/tutorial-list.component.vue')
+      component: TutorialsComponent
     }
   ]
 })
