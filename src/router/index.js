304a43c6706bd4ad77838500e6faded4f64a25c9
Angel Velasquez <pcisavel@upc.edu.pe>
2022-09-21 11:43:39 -0500

    feat(ui): Main UI.

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/home',
       name: 'home',
       component: HomeView
     },
@@ -16,6 +16,11 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/tutorials',
+      name: 'tutorials',
+      component: () => import('../learning/pages/tutorial-list.component.vue')
     }
   ]
 })
